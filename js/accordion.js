d7448f94e7f935fc288b881825679151cf767ba2
OlyaDa <dashevska.olga@gmail.com>
2022-08-03 16:54:55 +0300

    edit overlay and accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,81 @@
 
+// class GraphAccordion {
+//     constructor(selector, options) {
+//         let defaultOptions = {
+//             isOpen: () => { },
+//             isClose: () => { },
+//             speed: 900
+//         };
+
+//         this.options = Object.assign(defaultOptions, options);
+//         this.accordion = document.querySelector(selector);
+//         this.control = this.accordion.querySelector('.accordion__control');
+//         this.content = this.accordion.querySelector('.accordion__content');
+//         this.event();
+//         this.start();
+//     }
+
+//     start() {
+//         console.log('start!');
+//         if (this.accordion) {
+//             if (this.accordion.classList.contains('is-open')) {
+//                 this.open()
+//             }
+//             else {
+//                 this.close();
+//             }
+//         }
+//     }
+
+//     event() {
+//         console.log('event!');
+
+//         if (this.accordion) {
+//             this.accordion.addEventListener('click', (e) => {
+//                 this.accordion.classList.toggle('open');
+
+//                 if (this.accordion.classList.contains('open')) {
+//                     this.open();
+//                 } else {
+//                     this.close();
+//                 }
+//             });
+//         }
+//     }
+
+//     open() {
+//         this.accordion.style.setProperty('--accordion-time', `${this.options.speed / 1000}s`);
+//         this.accordion.classList.add('is-open');
+//         this.control.setAttribute('aria-expanded', true);
+//         this.content.setAttribute('aria-hidden', false);
+//         // this.content.setAttribute('style', 'visibility: hidden;');
+//         this.content.style.visibility = 'visible';
+//         this.content.style.maxHeight = this.content.scrollHeight + 'px';
+//         this.options.isOpen(this);
+//     }
+
+//     close() {
+//         console.log('close');
+//         this.accordion.classList.remove('is-open');
+//         this.control.setAttribute('aria-expanded', false);
+//         this.content.setAttribute('aria-hidden', true);
+//         this.content.setAttribute('style', 'visibility: hidden;');
+//         this.content.style.maxHeight = '0px';
+//         this.options.isClose(this);
+//     }
+// }
+
+// if (document.querySelector('.we-offer')) {
+//     const accordion1 = new GraphAccordion('.accordion-1', {
+//         speed: 900
+//     });
+
+//     const accordion2 = new GraphAccordion('.accordion-2', {
+//         speed: 900
+//     });
+// }
+
+
 class GraphAccordion {
     constructor(selector, options) {
         let defaultOptions = {
@@ -12,19 +89,6 @@ class GraphAccordion {
         this.control = this.accordion.querySelector('.accordion__control');
         this.content = this.accordion.querySelector('.accordion__content');
         this.event();
-        this.start();
-    }
-
-    start() {
-        console.log('start!');
-        if (this.accordion) {
-            if (this.accordion.classList.contains('is-open')) {
-                this.open()
-            }
-            else {
-                this.close();
-            }
-        }
     }
 
     event() {
@@ -48,34 +112,23 @@ class GraphAccordion {
         this.accordion.classList.add('is-open');
         this.control.setAttribute('aria-expanded', true);
         this.content.setAttribute('aria-hidden', false);
-        // this.content.setAttribute('style', 'visibility: hidden;');
-        this.content.style.visibility = 'visible';
         this.content.style.maxHeight = this.content.scrollHeight + 'px';
         this.options.isOpen(this);
     }
 
     close() {
-        console.log('close');
         this.accordion.classList.remove('is-open');
         this.control.setAttribute('aria-expanded', false);
         this.content.setAttribute('aria-hidden', true);
-        this.content.setAttribute('style', 'visibility: hidden;');
-        this.content.style.maxHeight = '0px';
+        this.content.style.maxHeight = null;
         this.options.isClose(this);
     }
 }
 
-if (document.querySelector('.we-offer')) {
-    const accordion1 = new GraphAccordion('.accordion-1', {
-        speed: 300
-    });
+const accordion1 = new GraphAccordion('.accordion-1', {
+    speed: 500,
+});
 
-    const accordion2 = new GraphAccordion('.accordion-2', {
-        speed: 100300
-    });
-}
-
-
-
-
-
+const accordion2 = new GraphAccordion('.accordion-2', {
+    speed: 500
+});
